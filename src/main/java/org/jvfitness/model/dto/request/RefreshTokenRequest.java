618f61cb692ddb623e9220df3d40481fd0690046
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-11-12 19:48:32 +0400

    refresh token request attached

---
+++ src/main/java/org/jvfitness/model/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,17 @@
+package org.jvfitness.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RefreshTokenRequest {
+
+    @NotBlank(message = "Refresh token cannot be empty")
+    private String refreshToken;
+}
